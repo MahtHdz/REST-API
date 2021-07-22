65be33be385161c77847bf2c2acf12e83cd56993
Maht <maht_hernandez@outlook.com>
2021-07-22 14:59:44 -0500

    Staging first steps

---
+++ server/src/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Modules 
+import './dbConn';
+import path from 'path';
+import morgan from 'morgan';
+import express from 'express';
+//const passport = require('passport');
+
+/* 
+################# 
+####  INIT   ####    
+################# */
+const app = express();
+
+// Settings 
+const PORT = 3000;
+app.set('port', process.env.PORT || PORT);
+app.use(express.urlencoded({ extended: true }));
+
+// Middlewares 
+app.use(morgan('dev'));
+app.use(express.json());
+//app.use(passport.session());
+//app.use(passport.initialize());
+
+// Routes 
+//app.use('/google', require('./routes/google.routes'));
+// app.use('/license', require('./routes/license.routes'));
+
+// Static Files 
+app.use(express.static(path.join(__dirname,'public')));
+
+// Instance to run the server
+app.listen(app.get('port'), () => {
+   console.log(`Server on port ${app.get('port')}`);
+});
+
+module.exports = app;
+
+// app.use(express.urlencoded({extended:false}));
